648deada0ba9598d9203254dc78993aa51574c21
Chamidumilam387 <Chamidumilan387@gmail.com>
2025-04-13 21:28:37 +0530

    Update session.js

--- session.js
+++ session.js
@@ -43,7 +43,7 @@ module.exports = {
     SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~aIt0xLTR#1KVjqp9EPzMdQfn-09Ux62uJ48Jhm5wotLp_B5ov59o",
     GITHUB_TOKEN: process.env.GITHUB_TOKEN || "ISbY1Rwla2d1BQcWPgAQLiCDW4UbqP25TXQq",
     GITHUB_USERNAME: process.env.GITHUB_USERNAME || "Chamidumilam387",
-    SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
+    SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94762862143", "94758775628"],
     PORT: process.env.PORT || "8000",
     SESSION_DIR: process.env.SESSION_DIR || "session",
     FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. Rashmika\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n\ud83c\udf1f Premium Version | Advanced Features | Secure & Reliable \ud83c\udf1f\n\u00a9 2025 QUEEN ANJU XPRO. All rights reserved." 
